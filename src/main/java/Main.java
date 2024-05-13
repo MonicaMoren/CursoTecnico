b0a181ed5143f363de3c963a9dc83d8fc79ca3cc
Monica Moreno <169068483+MonicaMoren@users.noreply.github.com>
2024-05-13 22:24:49 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");    
+  System.out.println("  ");
+     System.out.println("!!!!!!!!!!!!!!!!!!");
+     System.out.println("! Relato Pessoal !");
+     System.out.println("!!!!!!!!!!!!!!!!!!");
+     System.out.println("  ");
+     System.out.println("Meu nome é Monica, estou no 1° ano do Ensino Medio, Cursando Tecnico de Desenvolvimento e Analise de Sistemas, em uma das minhas materias de tecnico tenho a materia logica computacional e é sobre está que vim falar.");
+     System.out.println("  ");
+    System.out.println("No inicío do ano falamos sobre tabela verdade, contendo muitos exercicios para fazer e que ajuda a gravar, no final do 1 trimestre entramos no algoritimo que é um tema que instrui bastante para saber o que fazer quando quer programar.");
+       System.out.println("  ");
+System.out.println("Agora no começo do 2 trimestre entramos na programação onde aprendemos a escrever um código facil e rapido, e com isso evolui em nossa aprendizagem.");
+     System.out.println("  ");
+  System.out.println("Gosto do curso e sinto que aos poucos estou aprendendo cada vez mais com ele a tecnica de programar.");
+     System.out.println("  ");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+  }
+
+}
